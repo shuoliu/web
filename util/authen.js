ca5112f8a43947e82dc2f5899dbb180686036bf1
Shuo <shuoliu.us@gmail.com>
2014-04-21 15:30:26 -0700

    Update authen.js

--- util/authen.js
+++ util/authen.js
@@ -1,6 +1,4 @@
-var md5 = require('./md5.js').MD5;
-//to be removed
-var validUser = {user:'5d1f6562596e77f7e2c96aa45b9a2cf9',username:'Shuo',password:'622175b7e566990297b6e6f98f878482'};
+
 
 function selectUser(user) {
 	user = md5(user);
@@ -14,9 +12,8 @@ function login (req, res) {
 	var user = req.body.username;
 	var psw = req.body.password;
 	var validUser;
-	if(user=='shuo' && psw=='bios1214') {
-		console.log(user + " login success!");
-		validUser = {username:"Shuo"};
+	if(user=='/**/' && psw=='/**/') {
+		validUser = {/**/};
 	}
 	var err = {};
 	if(validUser != undefined) {
